637224c7b553c7c1738cbb1d0a3e40aeb1ec2284
yexnsxo <yslove829@naver.com>
2025-10-29 00:27:12 +0900

    [Feat] 결과 화면 이동 페이지 구현 완료

--- src/router.jsx
+++ src/router.jsx
@@ -7,6 +7,7 @@ import Onboarding3 from "./pages/Onboarding/Onboarding3";
 import Onboarding4 from "./pages/Onboarding/Onboarding4";
 import Onboarding5 from "./pages/Onboarding/Onboarding5";
 import Loading from "./pages/Loading";
+import ResultPage from "./pages/ResultPage";
 import App from "./App.jsx";
 
 const router = createBrowserRouter([
@@ -22,6 +23,7 @@ const router = createBrowserRouter([
       { path: "onboarding4", element: <Onboarding4 /> },
       { path: "onboarding5", element: <Onboarding5 /> },
       { path: "loading", element: <Loading /> },
+      { path: "result", element: <ResultPage /> },
     ],
   },
 ]);
